9e71282764cbbd0fa1eb6d02365b435afed166dd
DevAthhh <atheros.company@gmail.com>
2024-05-31 18:14:50 +0300

    v2.0.0

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import telebot
 
-bot = telebot.telebot()
+bot = telebot.TeleBot('7300492927:AAF6BOO36Z_OyRRBXlNu-JqsaBK5PQqNP6Y')
+
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
