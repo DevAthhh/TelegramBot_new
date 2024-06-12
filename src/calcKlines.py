b741322a8d75f175930de24e774bed774dbd5d25
DevAthhh <atheros.company@gmail.com>
2024-06-12 12:24:13 +0300

    v2.1.0

--- src/calcKlines.py
+++ src/calcKlines.py
@@ -1,6 +1,7 @@
 import requests
 import time
-
+from datetime import datetime, timedelta
+import pytz # Для работы с временными зонами
 def get_price():
     type_coin = 'BTC'
 
@@ -23,13 +24,24 @@ def get_price():
                 return item['last_price']
     return "Цена не найдена"
 
+############################################################### Получение объемов с coingecko ###########################################################################
+def get_btc_volume():
+    url = "https://api.coingecko.com/api/v3/coins/bitcoin"
+    response = requests.get(url)
+    data = response.json()
+    return data['market_data']['total_volume']['usd']
+
+
+############################################################### Конец ###########################################################################
+
 def calc_klines():
     kline = {
         'open': 0,
         'high': 0,
         'low': 0,
         'close': 0,
-        'color': ''
+        'color': '',
+        'volume': 0
     }
 
     #   Recording primary data
@@ -38,9 +50,9 @@ def calc_klines():
     kline['high'] = price
     kline['low'] = price
     count_time = 0
+    temp_vol = get_btc_volume()
 
     while True:
-        start_time = time.time()
         price = get_price()
         if kline['high'] < price:
             kline['high'] = price
@@ -49,18 +61,24 @@ def calc_klines():
         count_time += 1
 
         print(f'High: {kline["high"]}\nLow: {kline["low"]}\n\n')
-        end_time = time.time()
-        print(end_time - start_time)
-        time.sleep(1.3)
+        time.sleep(2)
 
 ####################################################################### разделение между таймфреймами ######################################################################
 
         if count_time == 900:
             kline['close'] = get_price()
-            kline['color'] = 'red' if kline['close'] < kline['open'] else 'green'
+            kline['color'] = 'RED' if kline['close'] < kline['open'] else 'GREEN'
+
+            #   Recording volume
+            kline['volume'] = get_btc_volume() - temp_vol
+            temp_vol = get_btc_volume()
+
             print(kline)
-            count_time
+            count_time = 0
             with open('drb/_klines.cat', 'a+') as fr:
                 fr.write(str(kline) + '\n')
+                fr.close()
+            
         continue
+
 calc_klines()
