fabe4e4cab064d66876b9e0b46087aeb6dd6c7e4
DevAthhh <atheros.company@gmail.com>
2024-06-12 19:48:01 +0300

    v2.1.0 - a.k.a казино рф

--- src/solves.py
+++ src/solves.py
@@ -1,127 +1,124 @@
-# Изначальный коэффициент
-coefficient = 0
+def oracle_move():
+    # Изначальный коэффициент
+    coefficient = 0
 
-# Словарь для хранения оценок
-ratings = {
-    "veryGood": 4,
-    "good": 2,
-    "ok": 0,
-    "bad": -2,
-    "veryBad": -4
-}
-kline = {
-    'open': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
-    'high': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
-    'low': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
-    'close': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
-    'color': ['GREEN', 'RED', 'GREEN', 'RED', 'GREEN', 'GREEN', 'GREEN']
-}
-              
-# Функция для расчета тренда
-if len(kline['close']) < 25:
-    raise ValueError("Недостаточно данных для расчета тренда")
+    # Словарь для хранения оценок
+    ratings = {
+        "veryGood": 4,
+        "good": 2,
+        "ok": 0,
+        "bad": -2,
+        "veryBad": -4
+    }
 
-if kline['close'][25] > kline['close'][1]:
+    kline = 0.0
+
+    try: 
+        with open('drb/_klines.cat', 'r') as fr:
+            str_kline = fr.readlines()
+            for i in range(len(str_kline)):
+                str_kline[i] = eval(str_kline[i])
+            kline = str_kline
+            fr.close()
+            kline.reverse()
+        for i in range(len(kline)):
+            kline[i]['open'] = float(kline[i]['open'])
+            kline[i]['close'] = float(kline[i]['close'])
+            kline[i]['high'] = float(kline[i]['high'])
+            kline[i]['low'] = float(kline[i]['low'])
+            kline[i]['volume'] = float(kline[i]['volume'])
+        print(kline)
+    except Exception as e:
+        print('Попытка получить данные не увенчалась успехом')
+                
+    # Функция для расчета тренда
     trend = 'DOWN'
-elif kline['close'][25] < kline['close'][1]:
-    trend = 'UP'
-else:
-    trend = 'NEUTRAL'
-
-# Функция для расчета потенциала
-if len(kline['close']) < 4:
-    raise ValueError("Недостаточно данных для расчета потенциала")
-
-if kline['close'][4] > kline['close'][1]:
-    potential = 'DOWN'
-elif kline['close'][4] < kline['close'][1]:
-    potential = 'UP'
-else:
-    potential = 'NEUTRAL'
-
-# Коэффициент в зависимости от тренда и потенциала
-if trend == potential == 'UP':
-    coefficient += ratings['veryGood']
-elif trend == potential == 'DOWN':
-    coefficient += ratings['veryBad']
-elif trend != potential and potential == 'UP':
-    coefficient += ratings['good']
-elif trend != potential and potential == 'DOWN':
-    coefficient += ratings['bad']
-elif trend == potential == 'NEUTRAL':
-    coefficient += ratings['ok']
-
-# Коэффициент в зависимости от областей поддержки и сопротивления
-# if kline[1]['color'] == 'GREEN' and # ОБЛАСТЬ СОПРОТИВЛЕНИЯ - 10 < klines[1]['close'] < ОБЛАСТЬ СОПРОТИВЛЕНИЯ + 10: 
-#     coefficient += ratings['bad']
-# elif kline[1]['color'] == 'RED' and # ОБЛАСТЬ ПОДДЕРЖКИ + 10 < klines[1]['close'] < ОБЛАСТЬ СОПРОТИВЛЕНИЯ - 10: 
-#     coefficient += ratings['good']
-
-# Тела последних
-body_1 = abs(kline['open'][1] - kline['close'][1]) # тело предпоследней свечи
-body_2 = abs(kline['open'][2] - kline['close'][2]) # тело пред предпоследней свечи
-
-# Тени предпоследней
-if kline['color'][1] == 'GREEN': # Бычья свеча (green)
-    upper_shadow = kline['high'][1] - kline['close'][1]
-    lower_shadow = kline['open'][1] - kline['low'][1]
-else:  # Медвежья свеча (красная)
-    upper_shadow = kline['high'][1] - kline['open'][1]
-    lower_shadow = kline['close'][1] - kline['low'][1]
-
-# Коэффициент в зависимости от теней
-if upper_shadow + lower_shadow < body_1 * .2 and kline['color'][1] == 'GREEN':
-    coefficient += ratings['veryGood']
-elif upper_shadow + lower_shadow < body_1 * .3 and kline['color'][1] == 'GREEN':
-    coefficient += ratings['good']
-elif upper_shadow + lower_shadow < body_1 * .3 and kline['color'][1] == 'RED':
-    coefficient += ratings['bad']
-elif upper_shadow + lower_shadow < body_1 * .3 and kline['color'][1] == 'RED':
-    coefficient += ratings['veryBad']
-
-# Коэффициент в зависимости от тела
-if body_1 > body_2 and kline['color'][1] == 'GREEN':
-    coefficient += ratings['veryGood']
-elif body_1 == body_2 and kline['color'][1] == 'GREEN':
-    coefficient += ratings['good']
-elif body_1 == body_2 and kline['color'][1] == 'RED':
-    coefficient += ratings['bad']
-elif body_1 > body_2 and kline['color'][1] == 'RED':
-    coefficient += ratings['veryBad']
-
-# Коэффициент в зависимости от объемов
-# if volume[1] >= volume[2] and kline['color'][1] == 'GREEN':
-#     coefficient += ratings['veryGood']
-# elif volume[2] - 10 < volume[1] < volume[2] and kline['color'][1] == 'GREEN':
-#     coefficient += ratings['good']
-# elif volume[1] >= volume[2] and kline['color'][1] == 'RED':
-#     coefficient += ratings['veryBad']
-# elif volume[2] - 10 < volume[1] < volume[2] and kline['color'][1] == 'RED':
-#     coefficient += ratings['bad']
-
-# Результат
-if coefficient > 0:
-    up_down = 'UP'
-    print("UP")
-elif coefficient < 0:
-    up_down = 'DOWN'
-    print("DOWN")
-
-# Расчет процента
-def calculate_success_probability(coefficient):
-    max_coeff = 16
-    min_coeff = -16
+    try:
+        if kline[25]['close'] > kline[1]['close']:
+            trend = 'DOWN'
+        elif kline[25]['close'] < kline[1]['close']:
+            trend = 'UP'
+        else:
+            trend = 'NEUTRAL'
+    except Exception as e:
+        print('Недостаточное количество данных для записи тренда')
     
-    if coefficient >= 0:
-        probability = (coefficient / max_coeff) * 100
-    else:
-        probability = (coefficient / min_coeff) * 100  
-    return probability
+    potential = 'DOWN'
+    # Функция для расчета потенциала
+    try:
+        if kline[4]['close'] > kline[1]['close']:
+            potential = 'DOWN'
+        elif kline[4]['close'] < kline[1]['close']:
+            potential = 'UP'
+        else:
+            potential = 'NEUTRAL'
+    except Exception as e:
+        print('Недостаточное количество данных для записи потенциала')
 
-# Example of usage
-probability = calculate_success_probability(coefficient)
-print(f"Coefficient: {coefficient}, Success Probability: {probability}%")
+    # Коэффициент в зависимости от тренда и потенциала
+    if trend == potential == 'UP':
+        coefficient += ratings['veryGood']
+    elif trend == potential == 'DOWN':
+        coefficient += ratings['veryBad']
+    elif trend != potential and potential == 'UP':
+        coefficient += ratings['good']
+    elif trend != potential and potential == 'DOWN':
+        coefficient += ratings['bad']
+    elif trend == potential == 'NEUTRAL':
+        coefficient += ratings['ok']
 
+    # Коэффициент в зависимости от областей поддержки и сопротивления
+    # if kline[1]['color'] == 'GREEN' and # ОБЛАСТЬ СОПРОТИВЛЕНИЯ - 10 < klines[1]['close'] < ОБЛАСТЬ СОПРОТИВЛЕНИЯ + 10: 
+    #     coefficient += ratings['bad']
+    # elif kline[1]['color'] == 'RED' and # ОБЛАСТЬ ПОДДЕРЖКИ + 10 < klines[1]['close'] < ОБЛАСТЬ СОПРОТИВЛЕНИЯ - 10: 
+    #     coefficient += ratings['good']
+
+    # Тела последних
+    body_1 = abs(kline[1]['open'] - kline[1]['close']) # тело предпоследней свечи
+    body_2 = abs(kline[2]['open'] - kline[2]['close']) # тело пред предпоследней свечи
+
+    # Тени предпоследней
+    if kline[1]['color'] == 'GREEN': # Бычья свеча (green)
+        upper_shadow = kline[1]['high'] - kline[1]['close']
+        lower_shadow = kline[1]['open'] - kline[1]['low']
+    else:  # Медвежья свеча (красная)
+        upper_shadow = kline[1]['high'] - kline[1]['open']
+        lower_shadow = kline[1]['close'] - kline[1]['low']
+
+    # Коэффициент в зависимости от теней
+    if upper_shadow + lower_shadow < body_1 * .2 and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['veryGood']
+    elif upper_shadow + lower_shadow < body_1 * .3 and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['good']
+    elif upper_shadow + lower_shadow < body_1 * .3 and kline[1]['color'] == 'RED':
+        coefficient += ratings['bad']
+    elif upper_shadow + lower_shadow < body_1 * .3 and kline[1]['color'] == 'RED':
+        coefficient += ratings['veryBad']
+
+    # Коэффициент в зависимости от тела
+    if body_1 > body_2 and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['veryGood']
+    elif body_1 == body_2 and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['good']
+    elif body_1 == body_2 and kline[1]['color'] == 'RED':
+        coefficient += ratings['bad']
+    elif body_1 > body_2 and kline[1]['color'] == 'RED':
+        coefficient += ratings['veryBad']
+
+    if kline[1]['volume'] >= kline[2]['volume'] and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['veryGood']
+    elif kline[2]['volume'] - 10 < kline[1]['volume'] < kline[2]['volume'] and kline[1]['color'] == 'GREEN':
+        coefficient += ratings['good']
+    elif kline[1]['volume'] >= kline[2]['volume'] and kline['color'][1] == 'RED':
+        coefficient += ratings['veryBad']
+    elif kline[2]['volume'] - 10 < kline[1]['volume'] < kline[2]['volume'] and kline[1]['color'] == 'RED':
+        coefficient += ratings['bad']
+
+    # Результат
+    if coefficient > 0:
+        return 'UP'
+    elif coefficient < 0:
+        return 'DOWN'
 
 
 
@@ -137,3 +134,5 @@ print(f"Coefficient: {coefficient}, Success Probability: {probability}%")
 # Stochastic Oscillator
 # ADX (Average Directional Movement Index)
 # Объемы (Rolling average for smoothing)
+
+print(oracle_move())
